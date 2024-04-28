602d54627d8005ad490747de33bbdb5c4a613ba1
SpectralPixel <encrypteddvjjrxv@gmail.com>
2024-04-28 18:36:26 +0200

    Get currently opened workspace With multiple screens, the focused workspace is probably on the screen that the mouse is on or was last clicked. I'm unsure about this behavious since I only use one screen, if you have any problems with weird behaviour caused by this make sure to open an issue.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
     let mut connection = i3ipc::I3Connection::connect().unwrap();
-    let workspaces = connection.get_workspaces().unwrap();
+    let workspaces = connection.get_workspaces().unwrap().workspaces;
+    let focused_ws = workspaces.into_iter().find(|ws| ws.focused).unwrap();
 
-    // debug, see if workspaces are gotten correctly
-    for w in workspaces.workspaces {
-        println!("workspace: {} - {}", w.num, w.name);
-    }
+    // debug
+    println!("{}", focused_ws.name);
 }
