0317434fd1c3d9102cfc069bac5d92d8e6b16aa8
SpectralPixel <encrypteddvjjrxv@gmail.com>
2024-04-28 18:21:05 +0200

    Get workspaces from i3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    let connection = i3ipc::I3Connection::connect().unwrap();
+    let mut connection = i3ipc::I3Connection::connect().unwrap();
+    let workspaces = connection.get_workspaces().unwrap();
+
+    // debug, see if workspaces are gotten correctly
+    for w in workspaces.workspaces {
+        println!("workspace: {} - {}", w.num, w.name);
+    }
 }
